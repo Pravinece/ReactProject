e24ebd8dd835a6ce174a19f844956b89fb857b6c
Pravinece <pravinece2020@gmail.com>
2024-04-22 18:07:19 +0530

    commit4

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
       {/* <Authentication/> */}
       <Router>
         <Routes>
-          <Route path="/ReactLogin" element={<Authentication />} />
+          <Route path="/" element={<Authentication />} />
+          <Route path="/ReactProject" element={<Authentication />} />
           {/* <Route path="/landingPage" element={<LandingPage />} /> */}
           <Route path="/home" element={<Choice />} />
 
